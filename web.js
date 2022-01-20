75f152c221ec24fff7f12a9a34f2049f9e25aa80
Meet <meetmewada107@gmail.com>
2022-01-20 19:46:57 +0530

    First commit for TalentEase

---
+++ web.js
@@ -0,0 +1,18 @@
+// function startA() {
+//     setInterval(scaleUpDown, );
+// }
+
+// function scaleUpDown() {
+//     let loginButton = document.getElementById("login_button");
+//     let temp = 0;
+//     for (let i=0; i) {
+//         if (temp == 0) {
+//             loginButton.style.transform = "scale(2,2)";
+//             temp = 1;
+//         } else {
+//             loginButton.style.transform = "scale(1,1)";
+//             temp = 0;
+//         }
+//     }
+
+// }
